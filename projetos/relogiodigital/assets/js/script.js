ae0d59599e510c673b510144c37bbabfb514498b
pedropebello <pedroichicv@gmail.com>
2025-06-22 21:22:32 -0300

    Initial commit

---
+++ projetos/relogiodigital/assets/js/script.js
@@ -0,0 +1,8 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    
+})
